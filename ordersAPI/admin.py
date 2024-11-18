66d6a2fc292fb1589ac549b07df99837b65c499c
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-11-18 11:05:24 -0600

    Delete ordersAPI directory

--- ordersAPI/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import OrderInfo, OrderItems, ActiveKitchenOrders
-
-admin.site.register(OrderInfo)
-admin.site.register(OrderItems)
-admin.site.register(ActiveKitchenOrders)
