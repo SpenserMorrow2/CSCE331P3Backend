70ea2937d65ae9e8659cc11dee30ce1e79422693
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-11-18 15:38:20 -0600

    Delete ordersAPI directory

--- ordersAPI/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import OrderInfo, OrderItems
-
-class OrderInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderInfo
-        fields = ['orderid', 'totalprice', 'date', 'time']
-
