eb00b4cd542ce55d89bd188145b03eb50f97ed93
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-11-18 19:31:40 -0600

    Update urls.py

--- Auth/urls.py
+++ Auth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('get-token', views.get_token_for_manager, name='get_token_for_manager'),
     path('logout', views.logout_user, name='logout_user'),
+    path('valid', views.validate_token, name='validate_token'),
 ]
