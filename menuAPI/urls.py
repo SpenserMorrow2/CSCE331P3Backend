2be2216ff2a04bd33cf93fc2bd85cc24b9624e6a
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-11-18 19:03:21 -0600

    Update urls.py for menu

--- menuAPI/urls.py
+++ menuAPI/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('items/', views.MenuItems, name = 'MenuItems'),
     path('item/<int:id>', views.MenuItemDetail, name = 'MenuItemDetail'),
     path('item/<int:id>/price', views.MenuItemPrice, name = 'MenuItemPrice'),
-    path('addItem/', views.addMenuItem, name = "addMenuItem"), 
+    path('addItem', views.addMenuItem, name = "addMenuItem"), 
     path('removeItem/<int:removalid>', views.removeMenuItem, name = 'removeMenuItem'),
 
 ]
