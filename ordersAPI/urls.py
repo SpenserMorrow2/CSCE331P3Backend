047a22d25dc19dbb05cfa13d324914b9b710b686
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-11-20 15:52:06 -0600

    Orders updated

---
+++ ordersAPI/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('createOrder', views.create_order, name = 'create_order'),
+    path('removeKitchenOrder/<int:orderID>', views.RemoveKitchenOrder, name = 'RemoveKitchenOrder'),
+    path('getKitchenOrders', views.get_kitchen_orders, name= 'getKitchenOrders'),
+    path('restockReport', views.restockReport, name = 'restock_report'),
+    path('salesReport/<str:start_d>/<str:end_d>', views.salesReport, name = 'sales_report'),
+    path('productUsageReport', views.product_Usage_Report, name = 'productUsageReport'),
+    path('xreport', views.X_report, name = 'x_report'),
+    path('zreport', views.Z_report, name = 'z_report'),
+]
