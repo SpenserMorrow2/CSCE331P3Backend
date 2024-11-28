492cbb2a5574dce832fe9fb0da565f2435cf9425
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-11-27 19:02:33 -0600

    Delete menuAPI directory

--- menuAPI/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem, ItemPrice, MenuCalories
-
-admin.site.register(MenuItem)
-admin.site.register(ItemPrice)
-admin.site.register(MenuCalories)
-
-
