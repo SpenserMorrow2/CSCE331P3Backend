2c9b611ef7eed371af76bfef44e667e139c6337c
SpenserMorrow2 <spenser.morrow@gmail.com>
2024-12-01 16:52:24 -0600

    Menu API Upload?

---
+++ menuAPI/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import MenuItem, ItemPrice, MenuCalories
+
+admin.site.register(MenuItem)
+admin.site.register(ItemPrice)
+admin.site.register(MenuCalories)
+
+
